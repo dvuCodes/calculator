c21b7acd61a34b5471ce688368255ab2e8f0fc8a
Dat-Vu <89580762+dvuCodes@users.noreply.github.com>
2023-08-16 21:22:41 +1000

    Created a variable to store out stored inputs

--- script.js
+++ script.js
@@ -1,27 +1,40 @@
 "use strict";
 const inputDisplayEl = document.getElementById("input-display");
 
+let displayValue = "";
+
 document.addEventListener("click", (e) => {
   const data = e.target.dataset;
-  const inputDisplayEl = document.getElementById("input-display");
+  const { operator, number } = data;
 
-  console.log(data);
+  // render the divide and multiply symbols
+  const symbol = {
+    "&divide;": " \u00f7",
+    "&times;": " \u00d7",
+  };
 
-  if (data.operator === "C") inputDisplayEl.textContent = "";
-  if (data.operator === "%") inputDisplayEl.textContent += " %";
-  if (data.operator === "&divide") inputDisplayEl.textContent += "&divide";
-  if (data.operator === "+") inputDisplayEl.textContent += " +";
-  if (data.operator === "-") inputDisplayEl.textContent += " -";
-  if (data.operator === "x") inputDisplayEl.textContent += " x";
-  if (data.number === "7") inputDisplayEl.textContent += " 7";
-  if (data.number === "8") inputDisplayEl.textContent += " 8";
-  if (data.number === "9") inputDisplayEl.textContent += " 9";
-  if (data.number === "6") inputDisplayEl.textContent += " 6";
-  if (data.number === "5") inputDisplayEl.textContent += " 5";
-  if (data.number === "4") inputDisplayEl.textContent += " 4";
-  if (data.number === "3") inputDisplayEl.textContent += " 3";
-  if (data.number === "2") inputDisplayEl.textContent += " 2";
-  if (data.number === "1") inputDisplayEl.textContent += " 1";
+  if (operator === "C") displayValue = "";
+  if (operator === "%") displayValue += " %";
+  if (operator === "/") displayValue += symbol["&divide;"];
+  if (operator === "+") displayValue += " +";
+  if (operator === "-") displayValue += " -";
+  if (operator === "x") displayValue += symbol["&times;"];
+  if (number === "7") displayValue += " 7";
+  if (number === "8") displayValue += " 8";
+  if (number === "9") displayValue += " 9";
+  if (number === "6") displayValue += " 6";
+  if (number === "5") displayValue += " 5";
+  if (number === "4") displayValue += " 4";
+  if (number === "3") displayValue += " 3";
+  if (number === "2") displayValue += " 2";
+  if (number === "1") displayValue += " 1";
+  if (operator === "=") operate;
+
+  if (e.target.dataset) {
+    inputDisplayEl.innerHTML = displayValue;
+  }
+
+  console.log(displayValue);
 });
 
 function operate(x, operator, y) {
